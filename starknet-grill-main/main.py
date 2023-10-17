0e08b3e8aea879797c7682d092326df53c0b711d
MoonNodes_Inc <86493242+IvanchikIvanov@users.noreply.github.com>
2023-10-18 01:27:30 +0500

    Add files via upload

---
+++ starknet-grill-main/main.py
@@ -0,0 +1,10 @@
+from asyncio import run
+from src.core import start
+from src.logger import logger
+
+if __name__ == '__main__':
+    logger.info(f"\n---This software is written by @flextive. All right are reserved. Current version: 0.1.1---\n\n"
+                f"Donate: 0x63f9716a17c751d97306289b22556b879ed8fb74")
+    run(start())
+    logger.info('Enter any key to exit')
+    input()
